b3c577362591228ca13500666e83615d02c17858
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-06-26 00:07:48 -0400

    Create Game scene

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,15 +1,15 @@
 import 'phaser';
-
+import logoImg from '../assets/logo.png';
 export default class GameScene extends Phaser.Scene {
   constructor () {
     super('Game');
   }
 
   preload () {
-    this.load.image('logo', 'assets/logo.png');
+    this.load.image('logo', logoImg);
   }
 
   create () {
-    this.add.image(window.innerWidth/2, window.innerHeight/2, 'logo');
+    this.add.image((window.innerWidth/2), (window.innerHeight/2), 'logo');
   }
 }
