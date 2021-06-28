4acd4cc8d5ecd90d3041e6f91f4435a4b092f4a5
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-06-28 09:35:31 -0400

    Preload the actor image

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -86,6 +86,7 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image('box', 'assets/ui/grey_box.png');
     this.load.image('checkedBox', 'assets/ui/blue_boxCheckmark.png');
     this.load.image('ground', 'assets/ground.png');
+    thi.load.spritesheet('actor', 'assets/actor.png');
     this.load.audio('bgMusic', ['assets/TownTheme.mp3']);
   }
 
