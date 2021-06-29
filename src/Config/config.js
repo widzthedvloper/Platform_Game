664a2acd50047e3abaeca964d5cb3a42ecce88d2
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-06-28 21:47:12 -0400

    Add physics property to the config object

--- src/Config/config.js
+++ src/Config/config.js
@@ -4,5 +4,8 @@ export default {
   type: Phaser.AUTO,
   parent: 'phaser-example',
   width: 800, // window.innerWidth,
-  height: 600 // window.innerHeight
+  height: 600, // window.innerHeight
+  physics: {
+            default: "arcade"
+  }
 };
