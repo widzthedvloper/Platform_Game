5870ad996ddc037428071570dd3b68e8c38c0608
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-06-29 23:23:09 -0400

    Add acknowledgement to the credit scene

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -8,7 +8,8 @@ export default class CreditsScene extends Phaser.Scene {
 
   create () {
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
+    this.madeByText = this.add.text(0, 0, 'Created By: Widzmarc Jean Nesly Phelle', { fontSize: '26px', fill: '#fff' });
+    this.acknowledgementText = this.add.text(0, 0, 'Acknowledgement: JavaScript Capstone Project by Microverse', { fontSize: '20px', fill: '#fff' });
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
 
     Phaser.Display.Align.In.Center(
@@ -21,13 +22,31 @@ export default class CreditsScene extends Phaser.Scene {
       this.zone
     );
 
+    Phaser.Display.Align.In.Center(
+      this.acknowledgementText,
+      this.zone
+    );
+
     this.madeByText.setY(1000);
+
+    this.acknowledgementText.setY(400);
 
     this.creditsTween = this.tweens.add({
       targets: this.creditsText,
       y: -100,
       ease: 'Power1',
       duration: 3000,
+      delay: 1000,
+      onComplete: function () {
+        this.destroy;
+      }
+    });
+
+    this.creditsTween = this.tweens.add({
+      targets: this.acknowledgementText,
+      y: -100,
+      ease: 'Power1',
+      duration: 6000,
       delay: 1000,
       onComplete: function () {
         this.destroy;
