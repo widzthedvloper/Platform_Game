9ebb8bf885f56b77dfdb063eaba7266f0b8ba7e5
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-06-30 19:20:01 -0400

    Complet the api

--- src/api.js
+++ src/api.js
@@ -1,11 +1,6 @@
-import 'regenerator-runtime/runtime';
-
-const myId = 'y3AqkaM7j6qpW6NV4xkJ';
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
-
 const getScores = async () => {
   try {
-    const apiResponse = await fetch(`${url}/games/${myId}/scores/`);
+    const apiResponse = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/y3AqkaM7j6qpW6NV4xkJ/scores/');
     const apiScore = await apiResponse.json();
     return apiScore.result;
   } catch (error) {
@@ -18,7 +13,7 @@ const sendScore = async (name, score) => {
   params.user = name;
   params.score = score;
   try {
-    const apiResponse = await fetch(`${url}/games/${myId}/scores/`, {
+    const apiResponse = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/y3AqkaM7j6qpW6NV4xkJ/scores/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json'},
       body: JSON.stringify(params),
